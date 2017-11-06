75ac3991899a23f2fc3765c22ae95e1af78d1fb1
Sartaj Chowdhury <SC509@ntrs.com>
2017-11-06 09:42:29 -0600

    fix default value

--- src/background.js
+++ src/background.js
@@ -1,36 +1,28 @@
 // From: https://stackoverflow.com/a/23341124
 
+function setIcon(value){
+  var path = (value)?"icons/icon-on.png":"icons/icon-off.png";
+  chrome.browserAction.setIcon({path: path});
+}
+
 function getToggle(callback) { // expects function(value){...}
   chrome.storage.local.get('toggle', function(data){
-    if(data.toggle === undefined) {
-      callback(true); // default value
-    } else {
-      callback(data.toggle);
-    }
+    if(data.toggle === undefined) callback(false); // default value
+    else callback(data.toggle);
   });
 }
 
 function setToggle(value, callback){ // expects function(){...}
   chrome.storage.local.set({toggle : value}, function(){
-    if(chrome.runtime.lastError) {
-      throw Error(chrome.runtime.lastError);
-    } else {
-      callback();
-    }
+    if(chrome.runtime.lastError) throw Error(chrome.runtime.lastError);
+    else callback(value);
   });
-}
-
-function setIcon(value){
-  var path = (value)?"icons/icon-on.png":"icons/icon-off.png";
-  chrome.browserAction.setIcon({path: path});
 }
 
 getToggle(setIcon); // Initial state
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   getToggle(function(toggle){
-    setToggle(!toggle, function(){
-      setIcon(!toggle);
-    });
+    setToggle(!toggle, setIcon);
   });
 });
